642412f18982990ff59deb914238fc330cb946b4
Elis Coimbra <eliscoimbrademoura831@gmail.com>
2023-11-16 22:13:25 -0300

    Exercício Refatoracao

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -1,67 +1,72 @@
-var TennisGame1 = function(player1Name, player2Name) {
-    this.m_score1 = 0;
-    this.m_score2 = 0;
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
-};
+class TennisGame1{
+    constructor(player1, player2){
+        this.player1 = player1;
+        this.player2 = player2
+        this.m_score1 = 0;
+        this.m_score2 = 0;
+    }
 
-TennisGame1.prototype.wonPoint = function(playerName) {
-    if (playerName === "player1")
-        this.m_score1 += 1;
-    else
-        this.m_score2 += 1;
-};
-
-TennisGame1.prototype.getScore = function() {
-    var score = "";
-    var tempScore = 0;
-    if (this.m_score1 === this.m_score2) {
-        switch (this.m_score1) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-        }
-    } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-        var minusResult = this.m_score1 - this.m_score2;
-        if (minusResult === 1) score = "Advantage player1";
-        else if (minusResult === -1) score = "Advantage player2";
-        else if (minusResult >= 2) score = "Win for player1";
-        else score = "Win for player2";
-    } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.m_score1;
-            else {
-                score += "-";
-                tempScore = this.m_score2;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
+    pontuarNoJogo(playerName){
+        if(playerName == 'player1'){
+            this.m_score1 +=1;
+        }else{
+            this.m_score2 +=1
         }
     }
-    return score;
-};
-
-if (typeof window === "undefined") {
-    module.exports = TennisGame1;
+    verificarEmpate(){
+        const pontuacoes = {
+            0: 'Love-All',
+            1: 'Fifteen-All',
+            2:  'Thirty-All',
+            3:   'Deuce' 
+        }
+        const score = this.m_score1 == this.m_score2 ? pontuacoes[this.m_score1] : 'Não estão Empatados';
+        return score  
+    }
+    verificarSeExisteVantagemDeAlgumJogadorMaiorQue4Pontos(){
+        const OptionsMinusResult ={
+            '1' :  "Advantage player 1",
+            '-1' : "Advantage player 2",
+            "2" : "Win Player 1",
+            "3": "Win Player 1",
+            "4": "Win Player 1"   
+        }
+         const minusResult = this.m_score1 - this.m_score2
+         const score = (this.m_score2 >= 4 || this.m_score1 >= 4) ? OptionsMinusResult[minusResult]: "Não há vantagem entre os players"
+         return score
+        }
 }
+
+//TESTE DAS FUNCIONALIDADES DOS MÉTODOS
+game1= new TennisGame1('Luana', 'Elis')
+game1.pontuarNoJogo('player1')
+game1.pontuarNoJogo("player2")
+game1.pontuarNoJogo('player2')
+game1.pontuarNoJogo('player2')
+game1.pontuarNoJogo('player2')
+console.log(game1.verificarEmpate());
+console.log(game1.verificarSeExisteVantagemDeAlgumJogadorMaiorQue4Pontos())
+console.log(game1)
+
+//Não consegui desenvolver a refatoração desse for
+// else {
+//     for (var i = 1; i < 3; i++) {
+//         if (i === 1) tempScore = this.m_score1;
+//         else {
+//             score += "-";
+//             tempScore = this.m_score2;
+//         }
+//         switch (tempScore) {
+//             case 0:
+//                 score += "Love";
+//                 break;
+//             case 1:
+//                 score += "Fifteen";
+//                 break;
+//             case 2:
+//                 score += "Thirty";
+//                 break;
+//             case 3:
+//                 score += "Forty";
+//                 break;
+//         }
